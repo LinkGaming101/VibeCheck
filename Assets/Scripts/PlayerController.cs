c496d998d4cc43741402838a6d9f981bf220c631
LinkGaming101 <bentleyt4@gmail.com>
2024-01-10 17:25:08 -0600

    player anim
    lets try this again I. Hate. Git.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
-    public Rigidbody rig;
+    public float moveSpeed = 1;
+    public float rotSpeed = 20f;
+
+    bool isWalking;
+
+    private Rigidbody rig;
+    Vector3 movement;
+    Quaternion rotation = Quaternion.identity;
+
     public bool inDialogue;
+
+    public Animator anim;
 
     [Header("Player Controls")]
     public KeyCode interactionKey;
     public float interactRange;
+    public KeyCode animCheer;
+    public KeyCode animCheck;
+    public KeyCode animHappy;
 
     [Header("Player Stats")]
     public int anxietyPoints;
@@ -22,14 +34,15 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Move();
-
-        Animate();
-
-        if(Input.GetKey(interactionKey))
+        if (Input.GetKey(interactionKey))
         {
             Interact();
         }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
     }
 
     void Move()
@@ -38,20 +51,50 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 dir = transform.right * x + transform.forward * z;
-        dir *= moveSpeed;
-        dir.y = rig.velocity.y;
+        bool xInputMade = !Mathf.Approximately(x, 0f); //sets true if horizontal input is approx 0
+        bool zInputMade = !Mathf.Approximately(z, 0f);
+        isWalking = xInputMade || zInputMade;
 
-        rig.velocity = dir;
+        movement.Set(x, 0f, z);
+        movement.Normalize();
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, rotSpeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredForward);
+
+        rig.MovePosition(rig.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rig.MoveRotation(rotation);
+
+        Animate();
+
     }
 
     void Animate()
     {
-        //Animation code goes here if any
+        if (isWalking)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if (Input.GetKey(animCheck))
+        {
+            anim.SetTrigger("Check");
+        }
+        if (Input.GetKey(animCheer))
+        {
+            anim.SetTrigger("Cheer");
+        }
+        if (Input.GetKey(animHappy))
+        {
+            anim.SetTrigger("Happy");
+        }
     }
 
     void Interact()
-    {        
+    {
         Ray ray = new Ray(transform.position + transform.forward, transform.forward);
         RaycastHit[] hits = Physics.SphereCastAll(ray, interactRange, 1 << 8);
 

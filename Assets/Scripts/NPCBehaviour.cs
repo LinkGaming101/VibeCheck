b0253b72d9bb89b2c5e5f62c2ecc3c6db0385b37
MarioMingoia <mariomingoia@hotmail.com>
2024-01-11 01:46:00 +0000

    dialgoueIntro
    Dialogue stuff

--- Assets/Scripts/NPCBehaviour.cs
+++ Assets/Scripts/NPCBehaviour.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class NPCBehaviour : MonoBehaviour
 {
+    [Header("NPC Info")]
+    public string npcName;
+    public string npcInstrument;
+    public string npcCharacteristic;
+
+
+    [SerializeField]
+    PlayerController pcScript;
+
+    [Header("Dialogue Stuff")]
+    public KeyCode dialogueKey;
+    public int dialogueOption;
+    public GameObject panelDialogue;
+    public TextMeshProUGUI playerQuestion;
+    public TextMeshProUGUI npcAnswer;
 
     void Start()
     {
@@ -12,7 +29,35 @@ public class NPCBehaviour : MonoBehaviour
 
     void Update()
     {
-        Animate();    
+        Animate();
+
+        if (pcScript != null)
+        {
+            if (pcScript.inDialogue == true)
+            {
+                panelDialogue.SetActive(true);
+
+                if (dialogueOption == 0)
+                {
+                    string text1 = "hello, my name is [PC Name], what is your name";
+                    text1 = text1.Replace("[PC Name]", pcScript.playerName);
+                    playerQuestion.text = text1;
+                    string text2 = "My name is [NPC Name]";
+                    text2 = text2.Replace("[NPC Name]", npcName);
+                    npcAnswer.text = text2;
+                    dialogueOption = 1;
+                }
+                if (Input.GetKeyDown(dialogueKey))
+                {
+                    nextLine();
+                }
+            }
+            else if (pcScript.inDialogue == false)
+            {
+                panelDialogue.SetActive(false);
+                pcScript = null;
+            }
+        }
     }
 
     void Animate()
@@ -20,9 +65,42 @@ public class NPCBehaviour : MonoBehaviour
         // animation code goes here for idle and constant if any
     }
 
-    public void Dialogue()
+    public void Dialogue(GameObject plr)
     {
+        pcScript = plr.GetComponent<PlayerController>();
+        dialogueOption = 0;
         //start the NPC dialogue
+        pcScript.inDialogue = true;
         Debug.Log("Dialogue started!");
+    }
+
+    public void nextLine()
+    {
+        switch (dialogueOption)
+        {
+            case 1:
+                string text1 = "Nice to meet you [NPC Name], what instrument do you like to play";
+                text1 = text1.Replace("[NPC Name]", npcName);
+                playerQuestion.text = text1;
+                string text2 = "I like to play [instrument]";
+                text2 = text2.Replace("[instrument]", npcInstrument);
+                npcAnswer.text = text2;
+                break;
+            case 2:
+                string text4 = "I need [instrument] in my band, tell me something about you";
+                text4 = text4.Replace("[instrument]", npcInstrument);
+                playerQuestion.text = text4;
+                string text5 = "I [characteristic]";
+                text5 = text5.Replace("[characteristic]", npcCharacteristic);
+                npcAnswer.text = text5;
+                break;
+            case 3:
+                playerQuestion.text = "Sounds interesting, let's fight";
+                npcAnswer.text = " ";
+                Debug.Log("starts battle");
+                break;
+        }
+
+        dialogueOption++;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,24 +12,44 @@ public class PlayerController : MonoBehaviour
     [Header("Player Controls")]
     public KeyCode interactionKey;
     public float interactRange;
+    public int interactLayer;
 
     [Header("Player Stats")]
     public int anxietyPoints;
 
+    [Header("Player Stats")]
+    public string playerName;
+    public GameObject player;
+
     void Start()
     {
         rig = GetComponent<Rigidbody>();
+
+        player = this.gameObject;
+
+        interactLayer = LayerMask.NameToLayer("Interactables");
     }
 
     void Update()
     {
-        Move();
+        if (inDialogue == false)
+        {
+            Move();
+        }
 
         Animate();
 
-        if(Input.GetKey(interactionKey))
+        if(Input.GetKeyDown(interactionKey))
         {
-            Interact();
+            if (!inDialogue)
+            {
+                Interact();
+            }
+            else if (inDialogue)
+            {
+                print("exit dialogue");
+                inDialogue = false;  
+            }
         }
     }
 
@@ -55,13 +76,17 @@ public class PlayerController : MonoBehaviour
         Ray ray = new Ray(transform.position + transform.forward, transform.forward);
         RaycastHit[] hits = Physics.SphereCastAll(ray, interactRange, 1 << 8);
 
-        foreach (RaycastHit hit in hits)
+        foreach (RaycastHit hit in hits.Where(hit => hit.transform.gameObject.layer == interactLayer))
         {
             //grab the NPCs dialogue function and run it
-            hit.collider.GetComponent<NPCBehaviour>()?.Dialogue();
+            hit.collider.GetComponent<NPCBehaviour>()?.Dialogue(player);
 
             // if interacting with item we want to grab the item
         }
     }
 
+    public void exitDialogue()
+    {
+        inDialogue = false;
+    }
 }
